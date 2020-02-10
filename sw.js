51d312ac46f0de7bc78fb9568e48e3a2dda0b46a
liaofuzhan <liaofuzhan@vip.taxchina.com>
2020-02-10 17:06:40 +0800

    Site updated: 2020-02-10 17:05:52

--- sw.js
+++ sw.js
@@ -26,13 +26,13 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/2114475547.html');
 
+  precacheUrls.push('/posts/3973006348.html');
+
   precacheUrls.push('/posts/712036056.html');
 
   precacheUrls.push('/posts/4067495121.html');
 
   precacheUrls.push('/posts/3564551888.html');
-
-  precacheUrls.push('/posts/2435254904.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
