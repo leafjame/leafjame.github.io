b2b0edab490fc07f2273f3d39deb27f94a9db32c
liaofuzhan <liaofuzhan@vip.taxchina.com>
2020-03-04 17:21:24 +0800

    Site updated: 2020-03-04 17:20:47

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 04 2020 13:49:45 GMT+0800 (GMT+08:00)
+// build time:Wed Mar 04 2020 17:20:40 GMT+0800 (GMT+08:00)
 const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:false,lrcType:0,audio:[{name:"无尽光芒",artist:"许巍",url:"https://www.liaofuzhan.com/music/无尽光芒.mp3",cover:"https://s2.ax1x.com/2019/10/03/uBPBHf.jpg"},{name:"夕阳中的城市",artist:"许巍",url:"https://www.liaofuzhan.com/music/夕阳中的城市.mp3",cover:"https://s2.ax1x.com/2019/10/03/uBPBHf.jpg"},{name:"心中的歌谣",artist:"许巍",url:"https://www.liaofuzhan.com/music/心中的歌谣.mp3",cover:"https://s2.ax1x.com/2019/10/03/uBPBHf.jpg"},{name:"远航",artist:"许巍",url:"https://www.liaofuzhan.com/music/远航.mp3",cover:"https://s2.ax1x.com/2019/10/03/uBPBHf.jpg"}]});
 //rebuild by neat 
