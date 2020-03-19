51574f273f5f868ed7278d4e0d71ec27453987b2
liaofuzhan <liaofuzhan@vip.taxchina.com>
2020-03-19 16:22:11 +0800

    Site updated: 2020-03-19 16:21:28

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 19 2020 16:19:18 GMT+0800 (GMT+08:00)
+// build time:Thu Mar 19 2020 16:21:19 GMT+0800 (GMT+08:00)
 const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:false,lrcType:0,audio:[{name:"无尽光芒",artist:"许巍",url:"https://cdn.jsdelivr.net/gh/leafjame/cdn/music/Endless_light.mp3",cover:"https://cdn.jsdelivr.net/gh/leafjame/cdn/img/articles/15/1.jpg"},{name:"夕阳中的城市",artist:"许巍",url:"https://cdn.jsdelivr.net/gh/leafjame/cdn/music/sunset_city.mp3",cover:"https://cdn.jsdelivr.net/gh/leafjame/cdn/img/articles/15/1.jpg"},{name:"心中的歌谣",artist:"许巍",url:"https://cdn.jsdelivr.net/gh/leafjame/cdn/music/heart_song.mp3",cover:"https://cdn.jsdelivr.net/gh/leafjame/cdn/img/articles/15/1.jpg"},{name:"远航",artist:"许巍",url:"https://cdn.jsdelivr.net/gh/leafjame/cdn/music/Voyage.mp3",cover:"https://cdn.jsdelivr.net/gh/leafjame/cdn/img/articles/15/1.jpg"}]});
 //rebuild by neat 
