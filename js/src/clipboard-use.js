3e983c3016b06d998eee733def585a405d330677
liaofuzhan <liaofuzhan@vip.taxchina.com>
2020-04-16 18:15:39 +0800

    Site updated: 2020-04-16 18:14:49

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 16 2020 18:08:33 GMT+0800 (GMT+08:00)
+// build time:Thu Apr 16 2020 18:14:39 GMT+0800 (GMT+08:00)
 !function(t,n,e){var i=function(){var t="";t+='<button class="btn-copy" data-clipboard-snippet="">';t+='  <i class="fa fa-clipboard"></i><span>复制</span>';t+="</button>";$(".highlight .code pre").before(t);new ClipboardJS(".btn-copy",{target:function(t){swal({title:"Copied!",text:"",icon:"success",timer:1500,button:true});return t.nextElementSibling}})};i()}(window,document);
 //rebuild by neat 
