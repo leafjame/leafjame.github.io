ec2fc0c9ed05a453fad1947b2c2d03f577f8169f
liaofuzhan <liaofuzhan@vip.taxchina.com>
2020-04-16 18:21:42 +0800

    Site updated: 2020-04-16 18:20:54

--- js/src/authority.js
+++ js/src/authority.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 16 2020 18:14:39 GMT+0800 (GMT+08:00)
+// build time:Thu Apr 16 2020 18:20:43 GMT+0800 (GMT+08:00)
 document.addEventListener("keydown",function(e){var t=navigator.platform.match("Mac")?e.metaKey:e.ctrlKey;if(e.keyCode==123){e.preventDefault()}else if(t&&e.keyCode==83){e.preventDefault()}else if(t&&e.keyCode==67){e.preventDefault()}else if(t&&e.keyCode==80){e.preventDefault()}else if(t&&e.shiftKey&&e.keyCode==73){e.preventDefault()}else if(t&&e.keyCode==85){e.preventDefault()}else if(t&&e.shiftKey||t&&e.altKey||e.shiftKey&&e.altKey){e.preventDefault()}else if(e.shiftKey&&e.keyCode==121){e.preventDefault()}},false);document.oncontextmenu=function(){return false};
 //rebuild by neat 
