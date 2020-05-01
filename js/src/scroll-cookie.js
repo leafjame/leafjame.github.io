64971ba0d09eeba11d2bbcebf54050423e59b38e
liaofuzhan <liaofuzhan@vip.taxchina.com>
2020-05-01 15:34:38 +0800

    Site updated: 2020-05-01 15:33:51

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri May 01 2020 14:41:03 GMT+0800 (GMT+08:00)
+// build time:Fri May 01 2020 15:33:42 GMT+0800 (GMT+08:00)
 $(document).ready(function(){scrollCookieJs()});function scrollCookieJs(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}}
 //rebuild by neat 
