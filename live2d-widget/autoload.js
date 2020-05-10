d4fbe3452cccfe87db3dca1ac2f48b9a1194e337
liaofuzhan <liaofuzhan@vip.taxchina.com>
2020-05-10 22:27:15 +0800

    Site updated: 2020-05-10 22:26:28

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,3 +1,3 @@
-// build time:Mon May 04 2020 22:13:54 GMT+0800 (GMT+08:00)
+// build time:Sun May 10 2020 22:26:09 GMT+0800 (GMT+08:00)
 const live2d_path="https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";$("<link>").attr({href:live2d_path+"waifu.css",rel:"stylesheet"}).appendTo("head");$.ajax({url:live2d_path+"live2d.min.js",dataType:"script",cache:true});$.ajax({url:live2d_path+"waifu-tips.js",dataType:"script",cache:true});$(window).on("load",function(){initWidget(live2d_path+"waifu-tips.json","https://live2d.fghrsh.net/api")});
 //rebuild by neat 
